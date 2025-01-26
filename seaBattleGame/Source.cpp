cd3dcafbe14928da9f74c11037714700023d1c1e
vvalentina01 <v.valentina.n@mail.ru>
2025-01-26 19:20:28 +0300

    Random generating of PC Playground

--- seaBattleGame/Source.cpp
+++ seaBattleGame/Source.cpp
@@ -20,13 +20,18 @@
 #define  _CRT_SECURE_NO_WARNINGS // игнорируем предупреждения о "небезопасных" функциях из Си
 #include <stdio.h> //аналог iostream ??
 #include <locale.h>  //в случае с++ включен в iostream, не требуется доп. подключение
+#include <stdlib.h>
+#include <time.h>
 
 
 const int SIZE = 10;
 const char ALPHABET_HEADER[] = "ABCDEFGHIJ";
 const int NUMERIC_HEADER[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-const int PLAYER = 1;
-const int PC = 0;
+const bool PC = 0;
+const bool PLAYER = 1;
+const bool HORIZONTAL = 0;
+const bool VERTICAL = 1;
+
 
 
 /*
@@ -39,15 +44,111 @@ void startMenu() {
 
 }*/
 
+bool checkoutShip(int n, int xy[4][2], char (*p)[SIZE]);
+bool checkoutBeside(int x, int y, char (*p)[SIZE]);
 
-void createPCPlayground(FILE* f)
+void createShip(int n, char (*p)[SIZE])
+{
+	int xy[4][2];
+
+	do {
+		srand(time(NULL) + n);
+		int orientation = rand() % 2;
+
+		if (orientation == VERTICAL)
+		{
+			xy[0][0] = rand() % SIZE;
+			xy[0][1] = rand() % (SIZE + 1 - n);
+		}
+		else if (orientation == HORIZONTAL)
+		{
+			xy[0][0] = rand() % (SIZE + 1 - n);
+			xy[0][1] = rand() % (SIZE);
+		}
+
+		for (int i = 1; i < n; i++)
+		{
+			if (orientation == VERTICAL)
+			{
+				xy[i][0] = xy[i - 1][0];
+				xy[i][1] = xy[i - 1][1] + 1;
+			}
+			else if (orientation == HORIZONTAL)
+			{
+				xy[i][0] = xy[i - 1][0] + 1;
+				xy[i][1] = xy[i - 1][1];
+			}
+		}
+	}
+	while (!checkoutShip(n, xy, p));
+	
+	for (int i = 0; i < n; i++)
+	{
+		p[xy[i][0]][xy[i][1]] = '#';
+	}
+}
+
+bool checkoutBeside(int x, int y, char (*p)[SIZE])
+{
+	char beside[9] = {};
+	int k = 0;
+	for (int i = x - 1; i <= x + 1; i++)
+	{
+		for (int j = y - 1; j <= y + 1; j++)
+		{
+			if (i >= 0 && i < SIZE && j >= 0 && j < SIZE)
+			{
+				beside[k] = p[i][j];
+			}
+			if (beside[k] == '#')
+			{
+				return false;
+			}
+			k++;
+		}
+	}
+	return true;
+}
+
+
+bool checkoutShip(int n, int xy[4][2], char (*p)[SIZE])
+{
+	for (int i = 0; i < n; i++)
+	{
+		int x = xy[i][0];
+		int y = xy[i][1];
+		if (!checkoutBeside(x, y, p))
+			return false;
+	}
+	return true;
+}
+
+
+void createPCPlayground(char (*p)[SIZE])
 {
 	/*рандомное генерирование поля противника*/
+	
+	createShip(4, p);
+	createShip(3, p);
+	createShip(3, p);
+	createShip(2, p);
+	createShip(2, p);
+	createShip(2, p);
+	createShip(1, p);
+	createShip(1, p);
+	createShip(1, p);
+	createShip(1, p);
+};
+
+void createPlayerPlayground()
+{
+	/*генерирование поля игрока*/
+	
 
 
 };
 
-void readPlayground(char* p, int who)
+void readPlayground(char (*p)[SIZE], bool who)
 {
 	FILE* f;
 
@@ -60,8 +161,8 @@ void readPlayground(char* p, int who)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			fscanf(f, "%c", p + i * SIZE + j);
-			if (p[i * SIZE + j] == '\n')
+			fscanf(f, "%c", p[i][j]);
+			if (p[i][j] == '\n')
 			{
 				j--;
 			}
@@ -70,7 +171,7 @@ void readPlayground(char* p, int who)
 	fclose(f);
 }
 
-void printPlayground(char* p, int who)
+void printPlayground(char (*p)[SIZE], bool who)
 {
 	FILE* f;
 
@@ -89,7 +190,7 @@ void printPlayground(char* p, int who)
 		printf("%d|", NUMERIC_HEADER[i]);
 		for (int j = 0; j < SIZE; j++)
 		{
-			printf("%c", p[i * SIZE + j]);
+			printf("%c", p[i][j]);
 		}
 		printf("|\n");
 	}
@@ -98,7 +199,7 @@ void printPlayground(char* p, int who)
 	fclose(f);
 }
 
-void savePlayground(char* p, int who)
+void savePlayground(char (*p)[SIZE], bool who)
 {
 	FILE* f;
 
@@ -111,7 +212,7 @@ void savePlayground(char* p, int who)
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
-			fprintf(f, "%c", p[i * SIZE + j]);
+			fprintf(f, "%c", p[i][j]);
 		}
 		fprintf(f, "\n");
 	}
@@ -124,17 +225,20 @@ int main()
 {
 	setlocale(LC_ALL, "Ru");
 
-	//char pcPlayground[SIZE][SIZE];
-	char playerPlayground[SIZE * (SIZE + 1)];
+	char pcPlayground[SIZE][SIZE];
+	char playerPlayground[SIZE][SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++)
+		{
+			pcPlayground[i][j] = ' ';
+		}
+	}
 
 	//test
+	createPCPlayground(pcPlayground);
+	savePlayground(pcPlayground, PC);
+	printPlayground(pcPlayground, PC);
 
-	readPlayground(playerPlayground, PLAYER);
-	printPlayground(playerPlayground, PLAYER);
-	playerPlayground[1] = 'X'; 
-	savePlayground(playerPlayground, PLAYER);
 
-	readPlayground(playerPlayground, PLAYER);
-	printPlayground(playerPlayground, PLAYER);
 
 }
